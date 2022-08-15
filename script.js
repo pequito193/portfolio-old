306dfe486a72497b4a8a82e1c0743999432e18dd
pequito193 <vitorspequito@gmail.com>
2022-08-15 16:14:18 +0100

    Add styling

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function changeTheme() {
     document.body.classList.toggle('body-light')
     document.body.classList.toggle('body-dark')
+    document.querySelector('.slider').classList.toggle('slider-move')
+    document.querySelector('.slider').classList.toggle('slider-return')
 }
 
 const button = document.querySelector('.dark-mode')
